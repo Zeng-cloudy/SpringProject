07e04867f649430b56087d81e80ce062bdd4befa
20399 <2039964180@qq.com>
2023-04-18 21:11:40 +0800

    ssh demo2

--- src/main/java/pojo/Brand.java
+++ src/main/java/pojo/Brand.java
@@ -5,5 +5,6 @@ public class Brand {
         System.out.println("6666666");
         System.out.println("dev update");
         System.out.println("ssh demo");
+        System.out.println("ssh demo2");
     }
 }
