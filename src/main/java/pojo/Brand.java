eba815566433355391e19e81eb1dc44d6a9ae5d6
20399 <2039964180@qq.com>
2023-04-18 21:08:57 +0800

    ssh demo

--- src/main/java/pojo/Brand.java
+++ src/main/java/pojo/Brand.java
@@ -4,5 +4,6 @@ public class Brand {
     public static void main(String[] args) {
         System.out.println("6666666");
         System.out.println("dev update");
+        System.out.println("ssh demo");
     }
 }
